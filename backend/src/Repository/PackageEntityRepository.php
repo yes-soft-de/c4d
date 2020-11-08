1fe61701f30c6bf00d738098c9d90f14226d38aa
ralsalloum <ramisalloum12@hotmail.com>
2020-11-09 00:19:15 +0200

    Package Entity was being added

---
+++ backend/src/Repository/PackageEntityRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\PackageEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method PackageEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PackageEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PackageEntity[]    findAll()
+ * @method PackageEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PackageEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PackageEntity::class);
+    }
+
+    public function getActivePackages()
+    {
+        return $this->createQueryBuilder('package')
+            ->select('package.id, package.name, package.cost, package.note, package.carCount, package.city, package.orderCount')
+            ->andWhere('package.status = :status')
+            ->setParameter('status', "active")
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /*
+    public function findOneBySomeField($value): ?PackageEntity
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
---
+++ backend/src/Request/PackageCreateRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Request;
+
+
+class PackageCreateRequest
+{
+    private $name;
+
+    private $cost;
+
+    private $note;
+
+    private $carCount;
+
+    private $city;
+
+    private $orderCount;
+
+    private $status;
+}
