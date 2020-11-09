830e5e315355f6dcc0918d533ddd159d41d2cd3a
ralsalloum <ramisalloum12@hotmail.com>
2020-11-09 14:05:00 +0200

    Subscription Entity was being added

---
+++ backend/src/Repository/SubscriptionEntityRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SubscriptionEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method SubscriptionEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SubscriptionEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SubscriptionEntity[]    findAll()
+ * @method SubscriptionEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SubscriptionEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SubscriptionEntity::class);
+    }
+
+    // /**
+    //  * @return SubscriptionEntity[] Returns an array of SubscriptionEntity objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SubscriptionEntity
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
