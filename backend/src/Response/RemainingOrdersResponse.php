285a275a3e4af853d61d6103a2882c4a6ffcb6ab
habibdevsy <alhabebcenter@hotmail.com>
2020-12-13 19:52:29 +0300

    update

---
+++ backend/src/Response/RemainingOrdersResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Response;
+
+class RemainingOrdersResponse
+{
+    public $packageID;
+    public $packagename;
+    public $subscriptionID;
+    public $remainingOrders;
+    public $subscriptionstatus;
+    public $subscriptionStartDate;
+    public $subscriptionEndDate;
+}
