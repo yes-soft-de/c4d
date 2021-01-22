c78aa9c827d21b0e992ff3c2e2aa755fcdb25a92
Kenan <kenanhussein1@gmail.com>
2021-01-22 03:09:47 +0200

    file upload system

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -15,4 +15,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
