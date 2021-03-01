e8697ad7581d9039d45e8481a75cb69ad3eded17
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-03-01 15:10:50 +0200

    corrected proportions of my plan lines

--- flutter/lib/module_plan/ui/state/plan_state_loaded.dart
+++ flutter/lib/module_plan/ui/state/plan_state_loaded.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:c4d/module_plan/model/active_plan_model.dart';
 import 'package:c4d/module_plan/ui/screen/plan_screen.dart';
 import 'package:c4d/module_plan/ui/state/plan_state.dart';
@@ -47,7 +45,8 @@ class PlanScreenStateLoaded extends PlanScreenState {
                         FaIcon(FontAwesomeIcons.sync),
                         Padding(
                           padding: const EdgeInsets.all(16.0),
-                          child: Text('${activePlanModel.orders} Orders / Month'),
+                          child:
+                              Text('${activePlanModel.orders} Orders / Month'),
                         ),
                       ],
                     ),
@@ -57,97 +56,115 @@ class PlanScreenStateLoaded extends PlanScreenState {
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Flex(
-            direction: Axis.horizontal,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.compare_arrows),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Flex(
-                    direction: Axis.horizontal,
-                    children: [
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex: activePlanModel.orders,
-                        child: Container(
-                          height: 8,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex: (activePlanModel.orders -
-                                activePlanModel.activeOrders)
-                            .abs(),
-                        child: Container(
-                          height: 8,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Text('${activePlanModel.activeOrders} / '),
-              Text(
-                '${activePlanModel.orders}',
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Flex(
-            direction: Axis.horizontal,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.car_rental),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Flex(
-                    direction: Axis.horizontal,
-                    children: [
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex: activePlanModel.cars,
-                        child: Container(
-                          height: 8,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex:
-                            (activePlanModel.cars - activePlanModel.activeCars)
-                                .abs(),
-                        child: Container(
-                          height: 8,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Text('${activePlanModel.activeCars} / '),
-              Text(
-                '${activePlanModel.cars}',
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              ),
-            ],
-          ),
-        ),
+        _getOrderRow(
+            context,
+            activePlanModel.activeOrders,
+            (activePlanModel.orders - activePlanModel.activeOrders).abs(),
+            (activePlanModel.activeOrders / activePlanModel.orders) > 0.8),
+        _getCarsRow(
+            context,
+            activePlanModel.activeCars,
+            (activePlanModel.cars - activePlanModel.activeCars).abs(),
+            (activePlanModel.activeCars / activePlanModel.cars) > 0.8),
       ],
+    );
+  }
+
+  Widget _getOrderRow(BuildContext context, int filled, int empty,
+      [bool danger = false]) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.compare_arrows),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: filled,
+                    child: Container(
+                      height: 8,
+                      color: danger == true
+                          ? Theme.of(context).accentColor
+                          : Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: empty,
+                    child: Container(
+                      height: 8,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text('${activePlanModel.activeOrders} / '),
+          Text(
+            '${activePlanModel.orders}',
+            style: TextStyle(color: Theme.of(context).primaryColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getCarsRow(BuildContext context, int filled, int empty,
+      [bool danger = false]) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.car_rental),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: filled,
+                    child: Container(
+                      height: 8,
+                      color: danger == true
+                          ? Theme.of(context).accentColor
+                          : Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: empty,
+                    child: Container(
+                      height: 8,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text('${activePlanModel.activeCars} / '),
+          Text(
+            '${activePlanModel.cars}',
+            style: TextStyle(color: Theme.of(context).primaryColor),
+          ),
+        ],
+      ),
     );
   }
 }
