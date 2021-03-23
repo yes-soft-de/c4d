5a34d98ca5296ef50cd36b0a81ecd880ca2fcb6b
habibdevsy <habibdevsy>
2021-03-22 21:03:23 -0800

    add state for Vacations

--- backend/src/Entity/VacationsEntity.php
+++ backend/src/Entity/VacationsEntity.php
@@ -32,6 +32,11 @@ class VacationsEntity
      */
     private $endDate;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $state;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +74,18 @@ class VacationsEntity
     public function setEndDate($endDate): self
     {
         $this->endDate = new DateTime($endDate);
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(?string $state): self
+    {
+        $this->state = $state;
 
         return $this;
     }
