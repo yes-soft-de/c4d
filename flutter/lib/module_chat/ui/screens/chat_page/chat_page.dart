7b1fa18e472905ab5f2d363017a558d0cdf9daa9
yazan99sh <syria.yazan19@gmail.com>
2021-05-11 03:09:13 +0300

    some improvment

--- flutter/lib/module_chat/ui/screens/chat_page/chat_page.dart
+++ flutter/lib/module_chat/ui/screens/chat_page/chat_page.dart
@@ -33,8 +33,16 @@ class ChatPageState extends State<ChatPage> {
   bool initiated = false;
   bool support = false;
   bool feedBack = false;
+  bool down = true;
+  ScrollController scrollController;
   @override
   void initState() {
+    scrollController = ScrollController(initialScrollOffset: 0);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(Duration(milliseconds: 500)).whenComplete(() {
+        scrollController.jumpTo(scrollController.position.maxScrollExtent);
+      });
+    });
     widget._chatPageBloc.chatBlocStream.listen((event) {
       currentState = event.first;
       if (event.first == ChatPageBloc.STATUS_CODE_GOT_DATA) {
@@ -42,10 +50,24 @@ class ChatPageState extends State<ChatPage> {
         if (chatsMessagesWidgets.length == _chatMessagesList.length) {
         } else {
           buildMessagesList(_chatMessagesList).then((value) {
-            if (this.mounted) setState(() {});
+            if (this.mounted) {
+              setState(() {
+                down = true;
+              });
+            }
           });
         }
       }
+    });
+    scrollController.addListener(() {
+      setState(() {
+        if (scrollController.offset !=
+            scrollController.position.maxScrollExtent) {
+          down = true;
+        } else {
+          down = false;
+        }
+      });
     });
     super.initState();
   }
@@ -54,11 +76,11 @@ class ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     if (currentState == ChatPageBloc.STATUS_CODE_INIT) {
       chatRoomId = ModalRoute.of(context).settings.arguments;
-      if (chatRoomId.substring(0,2) == 'A#') {
+      if (chatRoomId.substring(0, 2) == 'A#') {
         chatRoomId = chatRoomId.substring(2);
         support = true;
       }
-      if (chatRoomId.substring(0,2) == 'F#') {
+      if (chatRoomId.substring(0, 2) == 'F#') {
         chatRoomId = chatRoomId.substring(2);
         feedBack = true;
       }
@@ -66,39 +88,83 @@ class ChatPageState extends State<ChatPage> {
     }
 
     return Scaffold(
-      body: Column(
-        // direction: Axis.vertical,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          AppBar(
-            title: Text(
-              S.of(context).chatRoom,
-              style: TextStyle(
-                color: Theme.of(context).brightness == Brightness.dark
-                    ? Colors.white
-                    : Colors.black,
+        body: Stack(
+      children: [
+        Column(
+          // direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            AppBar(
+              title: Text(
+                S.of(context).chatRoom,
+                style: TextStyle(
+                  color: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.white
+                      : Colors.black,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: chatsMessagesWidgets != null
-                ? ListView(
-                    children: chatsMessagesWidgets,
-                    reverse: false,
-                  )
-                : Center(
-                    child: Text(S.of(context).loading),
+            Expanded(
+              child: chatsMessagesWidgets != null
+                  ? ListView(
+                      controller: scrollController,
+                      children: chatsMessagesWidgets,
+                      reverse: false,
+                    )
+                  : ListView(
+                      children: [
+                        Center(
+                          child: Text(S.of(context).loading),
+                        )
+                      ],
+                    ),
+            ),
+            ChatWriterWidget(
+              onMessageSend: (msg) {
+                widget._chatPageBloc
+                    .sendMessage(chatRoomId, msg, support, feedBack);
+              },
+              uploadService: widget._uploadService,
+            ),
+          ],
+        ),
+        down
+            ? Positioned(
+                bottom: 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: InkWell(
+                    onTap: () {
+                      scrollController.jumpTo(
+                        scrollController.position.maxScrollExtent,
+                      );
+                      setState(() {
+                        down = false;
+                      });
+                    },
+                    borderRadius: BorderRadius.circular(50),
+                    child: Container(
+                      width: 25,
+                      height: 25,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                      child: Icon(
+                        Icons.arrow_drop_down,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.black
+                            : Colors.white,
+                      ),
+                    ),
                   ),
-          ),
-          ChatWriterWidget(
-            onMessageSend: (msg) {
-              widget._chatPageBloc.sendMessage(chatRoomId, msg,support,feedBack);
-            },
-            uploadService: widget._uploadService,
-          ),
-        ],
-      ),
-    );
+                ),
+              )
+            : Container(),
+      ],
+    ));
   }
 
   Future<void> buildMessagesList(List<ChatModel> chatList) async {
