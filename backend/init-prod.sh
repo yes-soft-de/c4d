9a50bd604b22d2957286cebcc03407c3b3e6e7e5
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-06-09 23:12:43 +0300

    fixing database name

--- backend/init-prod.sh
+++ backend/init-prod.sh
@@ -13,7 +13,7 @@ echo "$PUBLIC_PEM" | base64 --decode > config/jwt/public.pem
 # Write .env file
 echo "APP_ENV=dev" > .env
 echo "APP_SECRET=c495ab23a840189035a176258ba53b64" >> .env
-echo "DATABASE_URL=mysql://root:$MYSQL_PASSWORD@c4d-mysql:3306/c4d?serverVersion=5" >> .env
+echo "DATABASE_URL=mysql://root:$MYSQL_PASSWORD@c4d-mysql:3306/c4d-mysql?serverVersion=5" >> .env
 
 echo "JWT_SECRET_KEY=%kernel.project_dir%/config/jwt/private.pem" >> .env
 echo "JWT_PUBLIC_KEY=%kernel.project_dir%/config/jwt/public.pem" >> .env
